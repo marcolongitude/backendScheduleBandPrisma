417d7ae59337969c285315b9e2973622fb3690c9
MARCO AURELIO <marcocpdti@gmail.com>
2021-05-22 12:58:15 -0300

    Config routes

---
+++ src/routes.ts
@@ -0,0 +1,20 @@
+import { Request, Response, Router } from 'express';
+import userController from './controllers/UserController';
+import sessionController from './controllers/SessionController';
+
+import AuthMiddleware from './middlewares/auth';
+
+const routes = Router();
+
+routes.get('/', (req: Request, res: Response) => {
+    return res.json({message: 'Hello World!'});
+});
+
+routes.post('/users', userController.create);
+routes.post("/sessions", sessionController.store);
+
+routes.use(AuthMiddleware);
+routes.get('/users', userController.getAll);
+routes.put('/users', userController.update);
+
+export default routes;
