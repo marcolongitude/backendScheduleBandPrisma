0ed68220c70a5cfe50d143b2ea1d155885d3b515
MARCO AURELIO <marcocpdti@gmail.com>
2021-06-05 19:50:43 -0300

    Create route post bands

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import userController from './controllers/UserController';
 import sessionController from './controllers/SessionController';
 
 import AuthMiddleware from './middlewares/auth';
+import BandController from './controllers/BandController';
 
 const routes = Router();
 
@@ -14,9 +15,14 @@ routes.post('/users', userController.create);
 routes.post("/sessions", sessionController.store);
 
 routes.use(AuthMiddleware);
+
+//User routes
 routes.get('/users', userController.getAll);
 routes.get('/users/:id', userController.getById);
 routes.get('/users/:email', userController.getByEmail);
 routes.put('/users/:id', userController.update);
+
+//Band routes
+routes.post('/bands', BandController.create);
 
 export default routes;
